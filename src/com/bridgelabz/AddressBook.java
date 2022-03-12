82d66bda5136e75f6904412d427b81c9fcb0d338
Shubham <ssp671995@gmail.com>
2022-03-12 13:14:58 +0530

    MODIFIED: Added Menu for AddressBook Management

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,5 +1,46 @@
 package com.bridgelabz;
 
-public class AddressBook {
+import java.util.Scanner;
 
+public class AddressBook {
+    //Scanner object for User Input
+    Scanner sc = new Scanner(System.in);
+
+    //Created Contacts Object for Getter and Setter Methods
+    Contacts contacts = new Contacts();
+
+    //Method to Add New Contact to AddressBook
+    public void addNewContact() {
+        System.out.println("Enter the Contact Details -");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstName(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastname(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the Zip Code :");
+        contacts.setZip(sc.next());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNo(sc.next());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmailID(sc.next());
+    }
+
+    //Method to Show the Contact Details
+    public void displayContact() {
+        System.out.println("Contact Details -");
+        System.out.println("First Name : "+contacts.getFirstName());
+        System.out.println("Last Name : "+contacts.getLastname());
+        System.out.println("Address : "+contacts.getAddress());
+        System.out.println("City : "+contacts.getCity());
+        System.out.println("State : "+contacts.getState());
+        System.out.println("Zip Code : "+contacts.getZip());
+        System.out.println("Phone Number : "+contacts.getPhoneNo());
+        System.out.println("EMail ID : "+contacts.getEmailID());
+    }
 }
+
