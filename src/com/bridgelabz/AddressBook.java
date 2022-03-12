75a331813b9fceb3e741a9e5dd3a977b725ee511
sspgithub123 <99379178+sspgithub123@users.noreply.github.com>
2022-03-12 14:28:02 +0530

    Update AddressBook.java
    MODIFIED: Created Contact, AddressBook Class with Variables

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,5 +1,32 @@
 package com.bridgelabz;
+import java.util.Scanner;
 
 public class AddressBook {
+   //Scanner object for User Input
+    Scanner sc = new Scanner(System.in);
+
+    //Created Contacts Object for Getter and Setter Methods
+    Contacts contacts = new Contacts();
+
+    //Method to Add New Contact to AddressBook
+    public void addNewContact() {
+        System.out.println("Enter the Contact Details -");
+        System.out.println("Enter the First Name :");
+        contacts.setFirstName(sc.next());
+        System.out.println("Enter the Last Name :");
+        contacts.setLastname(sc.next());
+        System.out.println("Enter the Address :");
+        contacts.setAddress(sc.next());
+        System.out.println("Enter the City :");
+        contacts.setCity(sc.next());
+        System.out.println("Enter the State :");
+        contacts.setState(sc.next());
+        System.out.println("Enter the Zip Code :");
+        contacts.setZip(sc.next());
+        System.out.println("Enter the Phone Number :");
+        contacts.setPhoneNo(sc.next());
+        System.out.println("Enter the EMail ID :");
+        contacts.setEmailID(sc.next());
+    }
 
 }
